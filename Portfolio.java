ddaf0c7d0ff00f5b2def5c0cc3d403452e83307b
JoePerito <jt12peri@siena.edu>
2023-02-20 22:39:59 -0500

    first commit

--- Portfolio.java
+++ Portfolio.java
@@ -7,18 +7,18 @@
  */
 public class Portfolio
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // StringBuffer sb = new StringBuffer();
-        // sb.append(String.format("%6s%25s%10s%15s%15s%n", 
-                // "Symbol", "Name", "Shares", "@Price", "Total"));
-        // sb.append("-----------------------------------------------------------------------\n");
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("%6s%25s%10s%15s%15s%n", 
+                "Symbol", "Name", "Shares", "@Price", "Total"));
+        sb.append("-----------------------------------------------------------------------\n");
 
-        // for(StockHolding s : stocks){
-            // sb.append(s.toString());
-        // }
-        // return sb.toString();
-    // }
+        for(StockHolding s : stocks){
+            sb.append(s.toString());
+        }
+        return sb.toString();
+    }
 }
--- StockHolding.java
+++ StockHolding.java
@@ -6,11 +6,44 @@
  */
 public class StockHolding
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+    private String symbol;
+    private String name;
+    private int numShares;
+    private double price;
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        
+        return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n",symbol, name, numShares, price, numShares * price); 
+    }
+    
+    public StockHolding(String symbol,String name,int numShares, double price){
+        this.symbol = symbol;
+        this.name = name;
+        this.numShares = numShares;
+        this.price = price;
+    }
+    
+    public String getSymbol(){
+        return symbol;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getNumShares(){
+        return numShares;
+    }
+    public double getPrice(){
+        return price;
+    }
+    
+    public double byShares(int numShares, double price){
+        
+    }
+    
+    public double sellShares(int numShares, double price){
+        
+    }
+    
 }
